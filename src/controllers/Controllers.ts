059792232c26fc511f18bc60ece93455f683dc3b
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-05-29 03:17:24 +0100

    feat: add ConfirmAccount to routes

--- src/controllers/Controllers.ts
+++ src/controllers/Controllers.ts
@@ -2,5 +2,6 @@ import { Base } from "./Base";
 import { NotFound } from "./NotFound";
 import { SignIn } from "./SignIn";
 import { SignUp } from "./SignUp";
+import { ConfirmAccount } from "./auth/ConfirmAccount";
 
-export default [Base, SignUp, SignIn, NotFound];
+export default [Base, SignUp, SignIn, ConfirmAccount, NotFound];
