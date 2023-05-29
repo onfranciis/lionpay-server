3afd7f0bada30cf250a1e07f81694d50e0707b7d
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-05-29 03:16:51 +0100

    feat: create ConfirmAccounts

---
+++ src/controllers/auth/ConfirmAccount.ts
@@ -0,0 +1,43 @@
+import { Response } from "express";
+import { getDB } from "../../db/DB";
+import { ControllerType } from "../../types/ControllersTypes";
+
+export const ConfirmAccount: ControllerType = {
+  path: "/confirmation/:id",
+  method: "get",
+  handler: async (req, res) => {
+    const { id: confirmationId } = req.params;
+    if (!confirmationId) {
+      res.status(400).json({ message: "No id was attached" });
+    } else {
+      const result = await getDB().findOne({
+        "profile.confirmation_id": confirmationId,
+      });
+      if (!result) {
+        res.status(400).json({ message: "Invalid Id" });
+      } else {
+        if (confirmationId === result.profile.confirmation_id) {
+          if (result.profile.confirmedAccount) {
+            res.json({
+              message: "Email has already been confirmed",
+            });
+          } else {
+            updateConfirmationStatus(confirmationId, res);
+          }
+        } else {
+          res.status(400).json({ message: "Invalid Id" });
+        }
+      }
+    }
+  },
+};
+
+const updateConfirmationStatus = async (Id: string, res: Response) => {
+  await getDB().updateOne(
+    { "profile.confirmation_id": Id },
+    { $set: { "profile.confirmedAccount": true } }
+  );
+  res.json({
+    message: "Email has been confirmed successfully",
+  });
+};
